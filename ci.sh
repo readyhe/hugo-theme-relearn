67bf53c738eb57f702a341dcf511fa85927f4523
ready <1916700536@qq.com>
2022-09-15 12:04:17 +0800

    添加新的逻辑

--- ci.sh
+++ ci.sh
@@ -7,4 +7,4 @@ git remote remove origin
 git remote add origin git@github.com:readyhe/readyBlog.git
 # git branch -D feature/readyhe
 git push origin --delete feature/readyhe
-git push -u origin master:feature/readyhe
+git push -u origin -f master
